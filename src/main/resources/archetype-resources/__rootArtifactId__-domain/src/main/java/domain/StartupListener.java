20777c7b53d3030c7a8c1e54b64506c3b99186ab
Funky Gao <funky.gao@gmail.com>
2020-10-27 00:09:25 +0800

    what is IStartupListener

--- src/main/resources/archetype-resources/__rootArtifactId__-domain/src/main/java/domain/StartupListener.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-domain/src/main/java/domain/StartupListener.java
@@ -6,6 +6,9 @@ package ${package}.domain;
 import org.springframework.stereotype.Component;
 import io.github.dddplus.runtime.IStartupListener;
 
+/**
+ * DDDplus框架启动成功后的回调机制.
+ */
 @Component
 public class StartupListener implements IStartupListener {
 
